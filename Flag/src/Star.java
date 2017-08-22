619164a55946b9d8583dacb905164a488750b88a
ethanzh <ethanzh@10.244.15.62>
2017-08-22 08:20:15 -0700

    Trying to debug stars, something weird is going on even when using hard-coded values for coordinates

--- Flag/src/Star.java
+++ Flag/src/Star.java
@@ -3,6 +3,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.util.Arrays;
 
 import javax.swing.JApplet;
 
@@ -21,7 +22,10 @@ public class Star extends Flag {
 				if((i % 2 == 1 && j % 2 == 1) || (i % 2 == 0 && j % 2 == 0)) {
 //					g.fillRect((int) starX,(int) starY, 3, 3);
 					//Change function below to 10 points
-					g.fillPolygon(calcStarX(starX), calcStarY(starY), 5);
+					
+					System.out.println(Arrays.toString(calcStarX(starX)));
+					
+					//g.fillPolygon(calcStarX(starX), calcStarY(starY), 5);
 					
 				}
 				
@@ -41,8 +45,10 @@ public class Star extends Flag {
 //		x[2] = (int) 0;
 //		x[3] = -x[2];
 //		x[4] = -x[1];
+
+				
 		
-		//Test
+//		//Test
 		x[0] = 0;
 		x[1] = 0;
 		x[2] = 1;
@@ -51,17 +57,18 @@ public class Star extends Flag {
 		
 		
 		//Change to 10 indices
-		for(int n = 0; n < 5; n++) {
-			x[n] += i;
+		for(int n = 0; n <= 4; n++) {
+			x[n] *= i;
+			//System.out.println(x[n]);
 		}
-		System.out.println(x);
+		//System.out.println(x);
 		return x;
 	}
 	
 	public int[] calcStarY(int j) {
 		
 		//Change to 10 indices
-		int[] y = new int[5];
+		int[] y = new int[10];
 		
 //		y[0] = (int) (K*Math.sin(18.0*Math.PI/180));
 //		y[1] = y[1];
@@ -77,10 +84,9 @@ public class Star extends Flag {
 		y[4] = -1;
 		
 		//Change to 10 indices
-		for(int n = 0; n < 5; n++) {
-			y[n] += j;
+		for(int n = 0; n <= 4; n++) {
+			y[n] *= j;
 		}
-		System.out.println(y);
 		
 		return y;
 				
