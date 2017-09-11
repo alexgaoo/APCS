8b2eadd86b6108f16a561e47b03e69666275d213
Alex Gao <alexgao@alexs-mbp.attlocal.net>
2017-09-11 01:28:33 -0700

    beginning half

--- Homework/1.3HW.rkt
+++ Homework/1.3HW.rkt
@@ -4,12 +4,43 @@
 ;1.30
 (define (sum term a next b)
   (define (iter a result)
-    (if <??>
-        <??>
-        (iter <??> <??>)))
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
   
-  (iter <??> <??>))
+  (iter a 0))
+
+(define (inc n) (+ n 1))
+
+(define (cube x) (* x x x))
+
+(define (sum-cubes a b)
+  (sum cube a inc b))
+
+(trace sum-cubes)
+
 ;1.31a
+
+(define (product f a next b)
+    (if (> a b)
+        1
+        (* (f a) (product f (next a) next b))))
+
+(define (inc2 n)
+  (+ n 2))
+
+(define (self n)
+  n)
+
+(define (factorial n)
+  (product self 1 inc n))
+
+(define (pi n)
+  (define (pi-term x)
+    (/ (* 4.0 (* x x)) (- (* 4 (* x x)) 1)))
+  (* 2 (product pi-term 1 inc n)))
+
+    
 
 ;1.32 (iterative process only)
 
