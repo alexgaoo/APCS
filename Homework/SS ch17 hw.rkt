cb5e341e409cef04c2857320b4cde6782dcc592b
ethanzh <ethan.houston@gmail.com>
2017-09-13 10:58:21 -0700

    temp

---
+++ Homework/SS ch17 hw.rkt
@@ -0,0 +1,12 @@
+; Ethan Houston, Alex Gao
+; 17.1
+
+; 'Rod
+; 'Chris
+; '(Chris Colin Hugh Paul)
+; 'Rod
+; '(Rod Argent Chris White)
+;'(Rod Argent Chris White)
+;'((Rod Argent) (Chris White))
+; 'Chris
+; '(Colin Blunstone)
