1f45e6559476feee27bd828664b65d05fe4f6a11
ethanzh <ethan.houston@gmail.com>
2017-09-13 11:24:30 -0700

    Did 17.2

--- Homework/SS ch17 hw.rkt
+++ Homework/SS ch17 hw.rkt
@@ -10,3 +10,23 @@
 ;'((Rod Argent) (Chris White))
 ; 'Chris
 ; '(Colin Blunstone)
+; #f
+
+17.2
+(define (f1 a b)
+           (append (cdr a) (car b)))
+
+(define (f2 a b)
+           (cons (cdr a) (car b)))
+
+(define (f3 a b)
+  (append a a)
+  )
+
+(define (f4 a b)
+  (list (list (car a) (car b)) (append (cdr a) (cdr b))))
+  
+
+;17.3
+; Four procedures
+
